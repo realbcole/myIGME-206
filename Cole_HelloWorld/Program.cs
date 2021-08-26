6fc24894e91175a747d5badafbd8316b1065ea73
Brandon Cole <realbcole@gmail.com>
2021-08-26 18:24:45 -0400

    started and finished

---
+++ Cole_HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cole_HelloWorld  
+{
+    //Class: Program
+    //Author: Brandon Cole
+    //Purpose: Print my name and ask for users name and then print it
+    //Restrictions: None
+    class Program
+    {
+        //Method: Main
+        //Purpose: Print my name and ask for users name and then print it
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brandon Cole");
+            string sName = null;
+            Console.Write("Enter your name: ");
+            sName = Console.ReadLine();
+            Console.WriteLine("Your name is " + sName);
+        }
+    }
+}
