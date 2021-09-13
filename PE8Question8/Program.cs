a231f41c1b03661e0b40389d92a8071f8fb86f8c
Brandon Cole <realbcole@gmail.com>
2021-09-13 14:10:15 -0400

    finished

---
+++ PE8Question8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8Question8
+{
+    // Class Program
+    // Author: Brandon Cole
+    // Purpose: replace all "no"s in a string with "yes"
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: get string from user, replace all no's with yes and return
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string response = Console.ReadLine();
+            string[] result = response.Split(' ');
+            string final = "";
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i].ToLower() == "no")
+                {
+                    if (result[i].StartsWith("N"))
+                    {
+                        final += "Yes";
+                    }
+                    else
+                    {
+                        final += "yes";
+                    }
+                }
+                else
+                {
+                    final += result[i];
+                }
+                final += " ";
+            }
+            
+            Console.WriteLine(final);
+            
+            // couldn't figure out how to make it work with punctuation after the no
+        }
+    }
+}
