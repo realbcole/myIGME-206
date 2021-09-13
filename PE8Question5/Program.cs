d93e434d6f14db161d32498883599ae0927f30ba
Brandon Cole <realbcole@gmail.com>
2021-09-13 13:33:48 -0400

    finished

---
+++ PE8Question5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8Question5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = -1
+                , y = 1
+                , z;
+            double[,,] values3D;
+
+
+        }
+    }
+}
---
+++ PE8Question7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8Question7
+{
+    // Class Program
+    // Author: Brandon Cole
+    // Purpose: reverse string from user
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: get string from user and output the string in reverse order
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string response = Console.ReadLine();
+            char[] charResponse = response.ToCharArray();
+            string reverse = "";
+
+            for (int i = charResponse.Length - 1; i >= 0; i--)
+            {
+                reverse += charResponse[i];
+            }
+            
+            Console.WriteLine(reverse);
+        }
+    }
+}
