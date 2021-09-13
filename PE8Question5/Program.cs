30b2cf7a42f5a267c513a4477481393c20b74dc7
Brandon Cole <realbcole@gmail.com>
2021-09-13 14:38:09 -0400

    lost

--- PE8Question5/Program.cs
+++ PE8Question5/Program.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace PE8Question5
 {
+    // Class Program
+    // Author: Brandon Cole
+    // Purpose: calaculate values of x, y, and z based on equation
+    // Restrictions: None
     class Program
     {
+        // Method: Main
+        // Purpose: increment x and y to get values of z and store them in 3d array
+        // Restrictions: None
         static void Main(string[] args)
         {
             double x = -1
@@ -15,7 +22,15 @@ namespace PE8Question5
                 , z;
             double[,,] values3D;
 
+            while (x < 1)
+            {
+                z = 3 * (y * y) + 2 * x - 1;
 
+                x += .1;
+
+            }
+
+            //very lost on this one
         }
     }
 }
