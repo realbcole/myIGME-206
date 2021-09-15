5f8c13d0e2109581ccf5b22a88c45144b723ade7
Brandon Cole <realbcole@gmail.com>
2021-09-15 10:56:33 -0400

    finished

---
+++ PE9Question3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9Question3
+{
+    // Class: Program
+    // Author: Brandon Cole
+    // Purpose: Create delegate function to impersonate the console.readline() function when asking for user input
+    // Restrictions: None
+    class Program
+    {
+
+        delegate string ReadLine();
+
+        // Method: Main
+        // Purpose: Create delegate function to impersonate console.readline()
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            ReadLine readline;
+
+            readline = new ReadLine(Console.ReadLine);
+
+            Console.WriteLine("enter your name: ");
+            string response = readline();
+
+            Console.WriteLine(response);
+        }
+    }
+}
