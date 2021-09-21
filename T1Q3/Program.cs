9e3f19c0ff71d327efb01fd964b22f46a62ac2cd
Brandon Cole <realbcole@gmail.com>
2021-09-21 13:48:11 -0400

    finished

---
+++ T1Q3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1Q3
+{
+    // Class: Program
+    // Author: Brandon Cole
+    // Purpose: Create delegate function to impersonate the console.readline() function when asking for user input
+    // Restrictions: None
+    class Program
+    {
+        delegate string Readline();
+
+        // Method: Main
+        // Purpose: Create delegate function to impersonate console.readline()
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            Readline readline;
+
+            readline = new Readline(Console.ReadLine);
+
+            Console.WriteLine("Please enter a string: ");
+            string response = readline();
+
+            Console.WriteLine(response);
+        }
+    }
+}
