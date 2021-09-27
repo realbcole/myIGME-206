f4b5b3661263866fbe172c61eeb6a6693628769a
Brandon Cole <realbcole@gmail.com>
2021-09-27 16:48:34 -0400

    finished

---
+++ Traffic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    // Class: program
+    // Author: Brandon Cole
+    // Purpose: Reference vehicles.dll and make functions
+    // Restrictions: None
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        void AddPassenger(IPassengerCarrier obj)
+        {
+            obj.LoadPassenger();
+            Console.WriteLine(obj.ToString());
+        }
+    }
+}
--- Vehicles/Class1.cs
+++ Vehicles/Class1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Vehicles
 {
     public abstract class Vehicle
