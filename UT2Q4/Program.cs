5d20f9c79e1c1c288a42a316e7ae23a06a7cb162
Brandon Cole <realbcole@gmail.com>
2021-10-19 14:58:44 -0400

    finished

---
+++ UT2Q4/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT2Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+        public string PhoneNumber;
+        public abstract void Connect();
+        public abstract void Disconnect();
+    }
+
+    public class RotaryPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    public class Tardis : RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho { get; }
+
+        public string FemaleSideKick { get; }
+
+        public void TimeTravel()
+        {
+
+        }
+    }
+
+    public class PhoneBooth : PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor()
+        {
+
+        }
+
+        public void CloseDoor()
+        {
+
+        }
+    }
+
+    public class PushButtonPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    public interface PhoneInterface
+    {
+        void Answer();
+
+        void MakeCall();
+
+        void HangUp();
+
+    }
+}
---
+++ UT2Q9/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT2Q9
+{
+    //Class: Program
+    //Author: Brandon Cole
+    //Purpose: Create objects of each child class and demonstrate polymorphism
+    //Restrictions: None
+    class Program
+    {
+        //Method: Main
+        //Purpose: Create the two objects and call the same method with each object
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            Speedboat speedboat = new Speedboat();
+            Sailboat sailboat = new Sailboat();
+
+            MyMethod(speedboat);
+
+            MyMethod(sailboat);
+        }
+        //Method: MyMethod
+        //Purpose: Call steer to show that there are different implementations of it for each object type
+        //Restrictions: None
+        static void MyMethod(Boat boat)
+        {
+            boat.Steer();
+        }
+    }
+
+    public interface Drive
+    {
+        void StartEngine();
+    }
+
+    public interface Sail
+    {
+        void RaiseSail();
+    }
+
+    public abstract class Boat
+    {
+        public string brand;
+
+        public virtual void Steer()
+        {
+            
+        }
+
+        public abstract void PutIntoWater();
+        public abstract void TakeOutOfWater();
+
+    }
+
+    public class Speedboat : Boat, Drive
+    {
+        public int fuel;
+
+        public void StartEngine()
+        {
+
+        }
+
+        public override void Steer()
+        {
+            Console.WriteLine("Turning the wheel!");
+        }
+
+        public override void PutIntoWater()
+        {
+            
+        }
+
+        public override void TakeOutOfWater()
+        {
+            
+        }
+
+    }
+
+    public class Sailboat : Boat, Sail
+    {
+        public bool sailsRaised;
+
+        public void RaiseSail()
+        {
+
+        }
+
+        public override void Steer()
+        {
+            Console.WriteLine("Turning the sails!");
+        }
+
+        public override void PutIntoWater()
+        {
+
+        }
+
+        public override void TakeOutOfWater()
+        {
+
+        }
+
+    }
+}
